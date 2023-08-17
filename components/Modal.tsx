5f79c25406a0009f608f067a8a586e6eda7bf8d9
Centeno Jose Antonio <jocecito182@gmail.com>
2023-08-17 08:16:59 -0300

    Add Modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,8 +1,33 @@
-'use client'
-import { useCallback, useRef, ReactNode } from 'react'
-import { useRouter } from 'next/navigation'
-import Image from 'next/image'
+"use client";
+import { useCallback, useRef, ReactNode, ReactElement } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
-export const Modal = () => {
-    return <div>Modal</div>
-}
+export const Modal = ({ children }: { children: ReactNode }) => {
+  const overlay = useRef<HTMLDivElement>(null);
+  const wrapper = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  const handleClick = () => {};
+  const onDismiss = () => {};
+  return (
+    <div
+      ref={overlay}
+      className="modal"
+      onClick={handleClick}
+    >
+      <button
+        type="button"
+        onClick={onDismiss}
+        className="absolute top-4 right-8"
+      >
+        <Image
+          src="/close.svg"
+          alt="close image"
+          width={75}
+          height={75}
+        />
+      </button>
+      <div>{children}</div>
+    </div>
+  );
+};
